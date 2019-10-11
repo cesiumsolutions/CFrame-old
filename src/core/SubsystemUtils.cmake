137d07720977a6065fa54106a55dd54f57fdf171
csklu <csklu@cesiumsolutions.com>
2019-10-12 00:04:54 +0200

    FIX: SubsystemUtils

--- src/core/SubsystemUtils.cmake
+++ src/core/SubsystemUtils.cmake
@@ -1,42 +1,49 @@
-
-
+# ------------------------------------------------------------------------------
+# @brief Includes the files for a subsystem corresponding to a specific stage.
 #
+# During CFrame processing, stages are different steps of processing that can
+# be called on a subsystem. These stages are typically triggered by a high level
+# call from a user. For example, the following stages are typically used:
 #
+# - Initialize:  Called on initial loading of the CFrame system.
+# - Preprocess:  Called when the cframe_preprocess() function is called.
+# - Process:     Called when the cframe_process() function is called.
+# - Postprocess: Called when the cframe_postprocess() function is called.
+# - tests:       Called if CFRAME_UNIT_TEST is ON.
 #
-function( cframe_subsystem_initialize name dir )
+# Subsystems can provide "hooks" into each of these stages by adding a file
+# called ${stage}.cmake in their corresponding directory, in which case this
+# file will be included, or by adding a subdirectory called ${stage} in their
+# directory in which case all of the files in that subdirectory will be included.
+# ------------------------------------------------------------------------------
+function( cframe_subsystem_stage name dir stage )
 
   cframe_message(
       MODE STATUS
       VERBOSITY 3
       TAGS CFrame
-      MESSAGE "Initializing Subsystem: ${name}"
+      MESSAGE "Subsystem: ${name} executing stage: ${stage} in dir: ${dir}"
   )
 
-
-  # Include all files in this directory (excluding directories and this file)
-  file(
-      GLOB entries
-      ${dir}/*
-  )
-  foreach( entry ${entries} )
-    if ( NOT IS_DIRECTORY ${entry} )
-      include( ${entry} )
-    endif()
-  endforeach()
-
-  if ( ${CFRAME_UNIT_TEST} )
-    file(
-        GLOB tests
-        ${dir}/tests/*.cmake
-    )
-    list( LENGTH tests numTests )
-    if ( ${numTests} GREATER 0 )
-      cframe_start_internal_tests()
-      foreach( test ${tests} )
-        include( ${test} )
-      endforeach()
-      cframe_finish_internal_tests()
-    endif()
+  # If there is a file in the subsystem's directory named ${stage}.cmake, just
+  # include that file.
+  if ( EXISTS dir/${stage}.cmake )
+    include( dir/${stage}.cmake )
+    return()
   endif()
 
-endfunction() # cframe_subsystem_initialize
+  # If there is a subdirectory in the subsystem's directory named ${stage},
+  # include all of the files in that subdirectory.
+  if ( IS_DIRECTORY dir/${stage} )
+    file(
+        GLOB entries
+        ${dir}/${stage}/*
+    )
+    foreach( entry ${entries} )
+      if ( NOT IS_DIRECTORY ${entry} )
+        include( ${entry} )
+      endif()
+    endforeach()
+  endif()
+
+endfunction() # cframe_exec_stage
